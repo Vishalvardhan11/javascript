807a41a8b31b34b940c221223be8568b0c92c4ad
Vishalvardhan11 <54879447+Vishalvardhan11@users.noreply.github.com>
2024-02-15 18:52:25 +0000

    dataTypes

---
+++ basic_01/01_variable.js
@@ -0,0 +1,9 @@
+const accountId="8327"
+let accountName="vishal"
+var accountPassword="1234"
+accountCity="patna"
+accountState=""
+
+//console.log(accountId);
+
+console.table([accountId,accountName,accountPassword,accountCity,accountState])
---
+++ basic_01/02_data_types.js
@@ -0,0 +1,11 @@
+"use strict" // treat all code as a newer version
+
+let name="vishal" //string
+let age= 26    //number
+let boolen=true // true/false
+
+
+
+console.log(typeof name,typeof age,typeof boolen);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
