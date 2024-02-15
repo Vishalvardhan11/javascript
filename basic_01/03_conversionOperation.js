3e9e9c0274b7f4584e6a3244db98535373006c6c
Vishalvardhan11 <54879447+Vishalvardhan11@users.noreply.github.com>
2024-02-15 19:50:24 +0000

    conversion

---
+++ basic_01/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score="55aa"
+
+console.log(score);
+
+let valueInNumber=Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+console.log(typeof NaN);
+
+let isLogedIn=1
+let logintype = Boolean(isLogedIn)
+console.log( typeof logintype);
+
+let someNumber=33
+
+let stringConv=String(someNumber)
+console.log(stringConv);
+console.log(typeof stringConv);
